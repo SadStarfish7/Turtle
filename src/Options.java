6b6a058ac6ea01282a4e2a799f6f3ba1fae355fe
Sad_Starfish <120674078+SadStarfish7@users.noreply.github.com>
2023-01-30 22:31:26 -0500

    Update Options.java
    No idea what I'm doing.  Putting all of my code in a comment for you guys to look at later.

--- src/Options.java
+++ src/Options.java
@@ -5,16 +5,24 @@ public class Options {
         //declare all static variables
         public static final String root = System.getProperty("user.dir")+"\\src\\";
         public static int screenWidth;
-
+        public static int screenHeight;
+        public static boolean gamePaused = false;
 
 
     public static void refreshOptions(){
         File file = new File(root+"options.txt");
 
     }
+        
+
+/*
+<USE A LISTENER TO CHECK FOR A SPECIFIC KEY PRESS, PROBABLY P, I DO NOT KNOW HOW TO DO THAT THOUGH>
+while (gamePaused == true) {
+        
+        
 }
-
-
+*/
+}
 
 
 
